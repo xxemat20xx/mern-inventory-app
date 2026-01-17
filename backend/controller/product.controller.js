4402d298bbf90a0e439c5d688386a6c137168021
xxemat20xx <62041802+xxemat20xx@users.noreply.github.com>
2026-01-17 22:51:24 +0800

    full CRUD backend

--- backend/controller/product.controller.js
+++ backend/controller/product.controller.js
@@ -68,7 +68,24 @@ export const deleteProduct = async ( req, res ) => {
 }
 
 // Get Single Product
-export const getProduct = async ( req, res ) => {}
-
+export const getProduct = async ( req, res ) => {
+    try {
+        const { id } = req.params;
+        const product = await Product.findById(id);
+        if(!product){
+            return res.status(404).json({ message: 'Product not found' });
+        }
+        res.status(200).json(product);
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+}
 // Get All Products
-export const getProducts = async ( req, res ) => {}
+export const getProducts = async ( req, res ) => {
+    try {
+        const products = await Product.find();
+        res.status(200).json(products);
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+}
