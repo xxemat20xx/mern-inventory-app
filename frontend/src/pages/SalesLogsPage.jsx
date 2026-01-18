0643648b33b5ed550c81c292102edae469c3722c
xxemat20xx <62041802+xxemat20xx@users.noreply.github.com>
2026-01-19 00:00:47 +0800

    commit pages

---
+++ frontend/src/pages/SalesLogsPage.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const SalesLogsPage = () => {
+  return (
+    <div>SalesLogsPage</div>
+  )
+}
+
+export default SalesLogsPage
---
+++ frontend/src/pages/StockLogsPage.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const StockLogsPage = () => {
+  return (
+    <div>StockLogsPage</div>
+  )
+}
+
+export default StockLogsPage
