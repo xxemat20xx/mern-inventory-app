99791c04c52c1f879ee3fc4c222280dc307a3e76
xxemat20xx <62041802+xxemat20xx@users.noreply.github.com>
2026-01-24 02:47:15 +0800

    commit new state

--- frontend/src/pages/Inventory.jsx
+++ frontend/src/pages/Inventory.jsx
@@ -1,8 +1,27 @@
-import React from 'react'
+// import { useState, useEffect } from 'react';
+import { 
+  Plus, 
+  Search, 
+  Edit3, 
+  Trash2, 
+  Filter, 
+  X,
+  AlertCircle,
+  PackageCheck
+} from 'lucide-react';
+import { useInventoryStore } from '../store/useInventoryStore';
+import { useEffect } from 'react';
 
 const Inventory = () => {
+  const { products, getProducts } = useInventoryStore();
+  useEffect(() => {
+    getProducts();
+  }, [getProducts]);
+  console.log(products);
   return (
-    <div>Inventory</div>
+    <div className='text-slate-50'>
+      <h1>Inventory</h1>
+    </div>
   )
 }
 
---
+++ frontend/src/store/useInventoryStore.js
@@ -0,0 +1,59 @@
+import { create } from "zustand";
+import api from '../api/api';
+
+export const useInventoryStore = create((set) => ({
+    products: [],
+    error: null,
+    isLoading: false,
+
+    setProducts: (products) => set({ products }),
+
+    createProduct: async (productData) => {
+        try {
+            const response = await api.post("/products/create", productData);
+            set((state) => ({
+                products: [...state.products, response.data],
+                isLoading: false,
+            }))
+        } catch (error) {
+            console.log(error);
+            set({ error: error.response.data.message, isLoading: false });
+        }
+    },
+    getProducts: async () => {
+    set({ isLoading: true, error: null });
+    try {
+        const response = await api.get('/products/get');
+        set({ products: response.data, isLoading: false });
+    } catch (error) {
+        console.log(error);
+        set({ error: error.response?.data?.message, isLoading: false });
+    }
+    },
+    updateProduct: async (productId, productData) => {
+        try {
+            const response = await api.put(`/products/update/${productId}`, productData);
+            set((state) => ({
+                products: state.products.map((product) => product._id === productId ? response.data : product),
+                isLoading: false,
+            }));
+        } catch (error) {
+            console.log(error);
+            set({ error: error.response.data.message, isLoading: false });
+        }
+    },
+    deleteProduct: async (productId) => {
+        try {
+            await api.delete(`/products/delete/${productId}`);
+            set((state) => ({
+                products: state.products.filter((product) => product._id !== productId),
+                isLoading: false,
+            }));
+        } catch (error) {
+            console.log(error);
+            set({ error: error.response.data.message, isLoading: false });
+        }
+    },
+    resetError: () => set({ error: null })
+
+}))
